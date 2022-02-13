3fec3c5c7a8031e10794a250b5000be26fdd914c
kei <rasmith106@outlook.com>
2022-02-13 14:37:18 -0500

    made auto timer, prints message once timer reaches 2sec

--- src/main/java/frc/robot/Auto.java
+++ src/main/java/frc/robot/Auto.java
@@ -1,16 +1,27 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Timer;
+
 public class Auto 
 {
-    
+    private static Timer autoTimer = new Timer();
+    private static boolean start;
+
     public static void autoInit()
     {
-
+        autoTimer.reset ();
     }
 
     public static void autoPeriodic()
     {
-
+        autoTimer.start ();
+        if (autoTimer.get()== 2.0)
+        {
+            autoTimer.stop();
+            System.out.println("Timer has stopped");
+        }
+        
+        start = false;
     }
     
 }
