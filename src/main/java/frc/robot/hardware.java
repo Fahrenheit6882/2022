cacf35cd2870a386dc2f6b55b8e1cb7f27fcf447
kei <rasmith106@outlook.com>
2022-02-13 13:45:43 -0500

    moved imports into hardware.java

--- src/main/java/frc/robot/hardware.java
+++ src/main/java/frc/robot/hardware.java
@@ -1,5 +1,11 @@
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPXConfiguration;
+
+import edu.wpi.first.wpilibj.Joystick;
+
 public class hardware 
 {
     // Use this Class for keeping hardware declarations in one place.  :)   
