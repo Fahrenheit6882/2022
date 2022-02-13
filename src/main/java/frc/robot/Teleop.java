130ae5bb3149afc539c1f0eef520c0e40c2b711a
Cassie <cassrobison@gmail.com>
2022-02-13 13:55:56 -0500

    Clean code :)

--- src/main/java/frc/robot/Teleop.java
+++ src/main/java/frc/robot/Teleop.java
@@ -1,30 +1,15 @@
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import com.ctre.phoenix.motorcontrol.can.VictorSPXConfiguration;
-
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
 
 public class Teleop {
- 
-       private static final Timer m_timer = new Timer();
   
     
      /** This function is run once each time the robot enters Teleop mode. */
     public static void teleopInit()
     {
-        m_timer.reset();
-        m_timer.start();
 
-                // Drive for 2 seconds
-    // if (m_timer.get() < 2.0) {
-    //     m_robotDrive.arcadeDrive(0.5, 0.0); // drive forwards half speed
-    //   } else {
-    //     m_robotDrive.stopMotor(); // stop robot
-    // }
-  }
+    }
 
     /** This function is called periodically during Teleop. */
     public static void teleopPeriodic()
