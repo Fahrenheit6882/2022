09a456b3244520b07661809a8e791f4fcc7b359d
Eowyn 3 <eaarendt@gmail.com>
2022-02-14 18:30:34 -0500

    added motor variables

--- src/main/java/frc/robot/Drive.java
+++ src/main/java/frc/robot/Drive.java
@@ -1,5 +1,12 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
+
 public class Drive {
-    
+ 
+private PWMVictorSPX leftMotor;
+private PWMVictorSPX rightMotor;
+{
+
+}
 }
