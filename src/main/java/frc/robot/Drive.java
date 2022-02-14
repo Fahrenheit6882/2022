d6c481c27ebd2afbdb9c4aa28dbb5f9238b52ea4
Eowyn 3 <eaarendt@gmail.com>
2022-02-14 18:38:08 -0500

    drive constructor and variable name adjust

--- src/main/java/frc/robot/Drive.java
+++ src/main/java/frc/robot/Drive.java
@@ -1,12 +1,14 @@
 package frc.robot;
-
 import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
 
 public class Drive {
- 
-private PWMVictorSPX leftMotor;
-private PWMVictorSPX rightMotor;
-{
+ //declaring variables for left and right motors
+private PWMVictorSPX leftMotorControler;
+private PWMVictorSPX rightMotorControler;
 
+void drive(double leftSpeed, double rightSpeed)
+{
+    
 }
+
 }
