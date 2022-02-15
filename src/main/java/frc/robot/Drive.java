aedf6cacda530d8627abd3550a84ae1c049e7ad6
Eowyn 3 <eaarendt@gmail.com>
2022-02-14 19:22:44 -0500

    adjest like whole thing to fit drive constructor, did stop in this one maybe?

--- src/main/java/frc/robot/Drive.java
+++ src/main/java/frc/robot/Drive.java
@@ -3,17 +3,27 @@ import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
 
 public class Drive {
  //declaring variables for left and right motors
-private PWMVictorSPX leftMotorControler = new PWMVictorSPX(0); //ports 0 and 1
-private PWMVictorSPX rightMotorControler = new PWMVictorSPX(2); //ports 2 and 3
+private PWMVictorSPX leftMotor = new PWMVictorSPX(0); //ports 0 and 1
+private PWMVictorSPX rightMotor = new PWMVictorSPX(2); //ports 2 and 3
 
-Drive(double leftSpeed, double rightSpeed)
+Drive(PWMVictorSPX leftMotorControler, PWMVictorSPX rightMotorControler)
 {
-    leftMotorControler.set(leftSpeed);
-    rightMotorControler.set(rightSpeed);
+ leftMotorControler = leftMotor;
+ rightMotorControler = rightMotor;
 }
+
+public void drive(double leftSpeed, double rightSpeed)
+{
+    leftMotor.set(leftSpeed);
+    rightMotor.set(rightSpeed);
+}
+
 
 public void Stop(double leftSpeed, double rightSpeed)
 {
-    
+    leftSpeed = 0;
+    rightSpeed = 0;
+    leftMotorControler.set(leftSpeed);
+    rightMotorControler.set(rightSpeed);
 }
 }
