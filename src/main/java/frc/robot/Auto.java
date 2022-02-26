3f912419403e86d4039ab6f6ade9f62e0f5d757d
Eowyn 3 <eaarendt@gmail.com>
2022-02-26 13:50:02 -0500

    Fixed code; used hardware drive for auto; auto 5 second drive forward

--- src/main/java/frc/robot/Auto.java
+++ src/main/java/frc/robot/Auto.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj.Timer;
 public class Auto 
 {
     private static Timer autoTimer = new Timer();
-    private static boolean start = false;
+    private static boolean started = false;
 
     public static void autoInit()
     {
@@ -14,17 +14,20 @@ public class Auto
 
     public static void autoPeriodic()
     {
-        //create and object for driving in auto
-        Drive autoDrive = new Drive(); //I'm stuck on this error here
-        if(start == false)
+        
+        if(started == false)
         { //starts time only once
             autoTimer.start();
-            start = true;
+            started = true;
         }
         //this lets it run for five-ish seconds where the robot goes forward at speed five and the hopefully stops
         if (autoTimer.get() <= 5.0)
         {
-            autoDrive.drive(0.5, 0.5);
+            hardware.drive.drive(0.5, 0.5);
+        }
+        else
+        {
+            hardware.drive.stop();
         }
         //I might need a line of code to stop the motors? I don't know for sure though
     }
